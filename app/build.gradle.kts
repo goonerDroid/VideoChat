c2e5e4a13d514daf8c2be330ff1a3de591239a95
williamj <williamj949@gmail.com>
2024-04-26 00:18:23 -0400

    Add top level deps, update libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization) apply false
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.google.gms) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.baseline.profile) apply false
 }
